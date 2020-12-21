6363185c197cd0ed142f09ad1d9e9da969faf4e0
Umit <ukaraogul@gmail.com>
2020-12-21 12:11:16 +0000

    folder structure is changed

---
+++ services/accounts/src/resolvers.js
@@ -0,0 +1,31 @@
+const resolvers = {
+  Query: {
+    me() {
+      return users[0];
+    },
+  },
+  User: {
+    __resolveReference(object) {
+      return users.find((user) => user.id === object.id);
+    },
+  },
+};
+
+const users = [
+  {
+    id: "1",
+    name: "Ada Lovelace",
+    birthDate: "1815-12-10",
+    username: "@ada",
+  },
+  {
+    id: "2",
+    name: "Alan Turing",
+    birthDate: "1912-06-23",
+    username: "@complete",
+  },
+];
+
+module.exports = {
+  resolvers,
+};
